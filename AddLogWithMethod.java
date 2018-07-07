b6c3fed6504781b14007964c4e75c7a6adc3e845
13593913496 <34449621+13593913496@users.noreply.github.com>
2018-07-07 14:41:13 +0800

    Update AddLogWithMethod.java
    v2.0

--- AddLogWithMethod.java
+++ AddLogWithMethod.java
@@ -1,7 +1,5 @@
-/**
- * 
- */
-package io.addlog;
+package test;
+
 
 import java.io.File;
 import java.io.Serializable;
@@ -12,109 +10,143 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 
 /**
  * @author chenli
- * ¸øÖ¸Â·¾¶ÏÂµÄ ËùÓĞjavaÎÄ¼şµÄËùÓĞ·½·¨¼ÓÉÏ logÈÕÖ¾,ÈÕÖ¾¼¶±ğTRACE
+ * ç»™æŒ‡è·¯å¾„ä¸‹çš„ æ‰€æœ‰javaæ–‡ä»¶çš„æ‰€æœ‰æ–¹æ³•åŠ ä¸Š logæ—¥å¿—,æ—¥å¿—çº§åˆ«TRACE
  */
 public class AddLogWithMethod {
-	//org.apache.logging.log4j.status ParameterizedBeanPropertyRowMapper
-	private static String filepath = "E:\\eclipse-workspace\\SpringSource\\src\\org\\springframework\\jdbc\\datasource";
-	
-	public static void main(String[] args) {
-				
-		File file = new File(filepath);	
+    
+    
+    //org.apache.logging.log4j.status ParameterizedBeanPropertyRowMapper
+    private static String filepath = 
+           "";
+    
+    public static void main(String[] args) {
+                
+        File file = new File(filepath); 
 
-		//»ñÈ¡ËùÓĞjavaÎÄ¼ş
-		File[] files = file.listFiles();
-		List<Code> codes = new ArrayList<Code>();
-		
-		if(files != null) {
-			for (File f : files) {
-				if(f.isFile() && f.getName().indexOf(".java") > -1 ) {
-					
-					codes.add( new Code(f.getName(), f.getAbsolutePath()) );
-				}
-			}
-		}else {
-			codes.add( new Code(file.getName(), file.getAbsolutePath()) );
-		}
-		
-		if(codes.size() > 0) {
-			ThreadPoolExecutor tpe = new ThreadPoolExecutor(5, 10, 1000, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100));
-			int size = codes.size();
-			
-			Map<String,Object> permap = null;
-			for (int i = 0; i < size; i++) {
-				permap= new HashMap<String,Object>();
-				permap.put(codes.get(i).getFilepath(), codes.get(i).getFilename());
-				tpe.execute(new ReadTask(permap, i));
-			}
-			//Èç¹ûÈÎÎñ¶¼ÒÑ¾­Íê³ÉÔò¹ØµôÏß³Ì³Ø
-			tpe.shutdown();
-			
-		}
+        //è·å–æ‰€æœ‰javaæ–‡ä»¶
+        File[] files = file.listFiles();
+        List<Code> codes = new ArrayList<Code>();
+        
+        if(files != null) {
+            recursiveFolder( files, codes );
+        }else {
+            codes.add( new Code(file.getName(), file.getAbsolutePath()) );
+        }
+        
+        if(codes.size() > 0) {
+            int num = 0;
+            ThreadPoolExecutor tpe = new ThreadPoolExecutor(5, 20, 2000L, TimeUnit.SECONDS, 
+                    new LinkedBlockingQueue<Runnable>(2000));
+            tpe.setRejectedExecutionHandler( new RejectedExecutionHandler()
+            {
+                
+                @Override
+                public void rejectedExecution( Runnable r, ThreadPoolExecutor executor )
+                {
+                 
+                }
+            } );
+            
+            try
+            {
 
-	}
-	
-	static class ReadTask implements Runnable{
-		
-		Map<String,Object> map ;
-		int n ;
-		public ReadTask(Map<String,Object> map , int n ) {
-			this.map = map;
-			this.n = n;
-		}
+                int size = codes.size();
+                
+                Map<String,Object> permap = null;
+                for (int i = 0; i < size; i++) {
+                    num = i;
+                    permap= new HashMap<String,Object>();
+                    permap.put(codes.get(i).getFilepath(), codes.get(i).getFilename());
+                    tpe.execute(new ReadTask(permap, i));
+                    
+                }
+                tpe.shutdown();
+            } catch ( Exception e )
+            {                
+                System.out.println("å¤„ç†æ‰å¼‚å¸¸ : " + num);
+                System.out.println(e.toString());
 
-		
-		private void process(Map<String, Object> map , int n) {			
-			Set<Entry<String, Object>> entry = map.entrySet();	
-			
-			for (Entry<String, Object> e : entry) {
-				ReadJavaCode r = new ReadJavaCode((String) e.getValue(), e.getKey() ,false );
-				r.init();
-			}
-		}
+            }
 
-		@Override
-		public void run() {
-			//µ±Ç°Åú´În
-			process(map,  n);
-		}
+        }
 
-	}
-	
-	static class Code implements Serializable{
+    }
+    //è¿­ä»£æ–‡ä»¶å¤¹
+    public static void recursiveFolder(File[] files,List<Code> codes) {
+        if(files == null || files.length == 0 ){
+            return;
+        }
+        for (File f : files) {
+            if(f.isFile() && f.getName().indexOf(".java") > -1 ) {                
+                codes.add( new Code(f.getName(), f.getAbsolutePath()) );
+            }else if(f.isDirectory()){
+                recursiveFolder( f.listFiles(), codes );
+            }
+        }        
+    }
+    
+    
+    static class ReadTask implements Runnable{
+        
+        Map<String,Object> map ;
+        int n ;
+        public ReadTask(Map<String,Object> map , int n ) {
+            this.map = map;
+            this.n = n;
+        }
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 4616676105884882896L;
-		private String filename;
-		private String filepath;
-		
-		public Code(String filename, String filepath) {
-			super();
-			this.filename = filename;
-			this.filepath = filepath;
-		}
-		
-		public String getFilename() {
-			return filename;
-		}
-		public void setFilename(String filename) {
-			this.filename = filename;
-		}
-		public String getFilepath() {
-			return filepath;
-		}
-		public void setFilepath(String filepath) {
-			this.filepath = filepath;
-		}
-		
-		
-	}
+        
+        private void process(Map<String, Object> map , int n) {         
+            Set<Entry<String, Object>> entry = map.entrySet();  
+            
+            for (Entry<String, Object> e : entry) {
+                ReadJavaCode r = new ReadJavaCode((String) e.getValue(), e.getKey() ,false );
+                r.init();
+            }
+        }
+
+        @Override
+        public void run() {
+            //å½“å‰æ‰¹æ¬¡n
+            process(map,  n);
+        }
+
+    }
+    
+    static class Code implements Serializable{
+
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 4616676105884882896L;
+        private String filename;
+        private String filepath;
+        
+        public Code(String filename, String filepath) {
+            super();
+            this.filename = filename;
+            this.filepath = filepath;
+        }
+        
+        public String getFilename() {
+            return filename;
+        }
+        public void setFilename(String filename) {
+            this.filename = filename;
+        }
+        public String getFilepath() {
+            return filepath;
+        }
+        public void setFilepath(String filepath) {
+            this.filepath = filepath;
+        }
+
+    }
 }
